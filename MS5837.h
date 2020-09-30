4c38bf8912b02ebba0bc69c2c7de9a1ba465ede3
Brenton Leighton <b.leighton@acfr.usyd.edu.au>
2020-09-30 16:23:11 +1000

    Split read function to avoid delays

--- MS5837.h
+++ MS5837.h
@@ -1,10 +1,10 @@
 /* Blue Robotics Arduino MS5837-30BA Pressure/Temperature Sensor Library
 ------------------------------------------------------------
- 
+
 Title: Blue Robotics Arduino MS5837-30BA Pressure/Temperature Sensor Library
 
 Description: This library provides utilities to communicate with and to
-read data from the Measurement Specialties MS5837-30BA pressure/temperature 
+read data from the Measurement Specialties MS5837-30BA pressure/temperature
 sensor.
 
 Authors: Rustom Jehangir, Blue Robotics Inc.
@@ -32,72 +32,82 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
--------------------------------*/ 
+-------------------------------*/
 
 #ifndef MS5837_H_BLUEROBOTICS
 #define MS5837_H_BLUEROBOTICS
 
 #include "Arduino.h"
 
-class MS5837 {
+class MS5837
+{
 public:
-	static const float Pa;
-	static const float bar;
-	static const float mbar;
+  static const float Pa;
+  static const float bar;
+  static const float mbar;
 
-	static const uint8_t MS5837_30BA;
-	static const uint8_t MS5837_02BA;
+  static const uint8_t MS5837_30BA;
+  static const uint8_t MS5837_02BA;
 
-	MS5837();
+  MS5837();
 
-	bool init();
+  bool init();
 
-	/** Set model of MS5837 sensor. Valid options are MS5837::MS5837_30BA (default)
-	 * and MS5837::MS5837_02BA.
-	 */
-	void setModel(uint8_t model);
+  /** Set model of MS5837 sensor. Valid options are MS5837::MS5837_30BA (default)
+   * and MS5837::MS5837_02BA.
+   */
+  void setModel(uint8_t model);
 
-	/** Provide the density of the working fluid in kg/m^3. Default is for 
-	 * seawater. Should be 997 for freshwater.
-	 */
-	void setFluidDensity(float density);
+  /** Provide the density of the working fluid in kg/m^3. Default is for
+   * seawater. Should be 997 for freshwater.
+   */
+  void setFluidDensity(float density);
 
-	/** The read from I2C takes up to 40 ms, so use sparingly is possible.
-	 */
-	void read();
+  /** Performs calculations per the sensor data sheet for conversion and
+   *  second order compensation.
+   */
+  void calculate();
 
-	/** Pressure returned in mbar or mbar*conversion rate.
-	 */
-	float pressure(float conversion = 1.0f);
+  /** The read from I2C takes up to 40 ms, so use sparingly is possible.
+   */
+  void read();
 
-	/** Temperature returned in deg C.
-	 */
-	float temperature();
+  /** These functions are the read function split into four function to avoid the 20 ms delays
+   *  The "read" functions must be called at least 20 ms after the "request conversion" functions
+   *  The "calculate" function must be called after the read functions, and before getting the values
+   */
+  void requestPressureConversion();
+  void requestTemperatureConversion();
+  void readPressure();
+  void readTemperature();
 
-	/** Depth returned in meters (valid for operation in incompressible
-	 *  liquids only. Uses density that is set for fresh or seawater.
-	 */
-	float depth();
+  /** Pressure returned in mbar or mbar*conversion rate.
+   */
+  float pressure(float conversion = 1.0f);
 
-	/** Altitude returned in meters (valid for operation in air only).
-	 */
-	float altitude();
+  /** Temperature returned in deg C.
+   */
+  float temperature();
+
+  /** Depth returned in meters (valid for operation in incompressible
+   *  liquids only. Uses density that is set for fresh or seawater.
+   */
+  float depth();
+
+  /** Altitude returned in meters (valid for operation in air only).
+   */
+  float altitude();
 
 private:
-	uint16_t C[8];
-	uint32_t D1, D2;
-	int32_t TEMP;
-	int32_t P;
-	uint8_t _model;
+  uint16_t C[8];
+  uint32_t D1, D2;
+  int32_t TEMP;
+  int32_t P;
+  uint8_t _model;
 
-	float fluidDensity;
+  float fluidDensity;
 
-	/** Performs calculations per the sensor data sheet for conversion and
-	 *  second order compensation.
-	 */
-	void calculate();
-
-	uint8_t crc4(uint16_t n_prom[]);
+  uint8_t crc4(uint16_t n_prom[]);
 };
 
 #endif
